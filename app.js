7b5786471bdc8340019e762fc755d8ce11558802
Drakunal <drakunaldey1@gmail.com>
2020-11-02 18:26:02 +0530

    accepts the device id manually and shows the sensor data, latitude and longitude

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const request = require('request')
 
-request.post('http://35.197.106.255:3000/api/v1.1/lastMultiple').form({
-    key: 'devid',
-    value: 'd8c68c44e70825b746c80c41add7279a'
+request.post('http://35.197.106.255:3000/api/v1.1/lastMultiple',(error,response,body)=>{
+
+  const data=JSON.parse(body)
+
+   console.log(data.data.id)
+   console.log(data.data.latitude)
+  
+   console.log(data.data.longitude)
+}).form({
+    
+    devid: 'd8c68c44e70825b746c80c41add7279a'
+  
   })

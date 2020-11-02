f446710e29cf319b42374e73f5a0c0b7194c7c77
Drakunal <drakunaldey1@gmail.com>
2020-11-02 23:09:16 +0530

    final submission

--- app.js
+++ app.js
@@ -4,18 +4,27 @@
 const request = require('request')
 
 const d=process.argv[2]
+if(!d)
+{
 
+
+  console.log("Enter the Device id")
+}
+else{
 request.post('http://35.197.106.255:3000/api/v1.1/lastMultiple',(error,response,body)=>{
 
   const data=JSON.parse(body)
-
-   console.log(data.data.id)
-   console.log(data.data.latitude)
   
-   console.log(data.data.longitude)
+    var s=data.data.id
+    var lat=data.data.latitude
+    var long=data.data.longitude
+   console.log(`The Sensor id is: ${s}`)
+   console.log(`The Latitude is: ${lat}`)
+  
+   console.log(`The Longitude is: ${long}`)
 }).form({
     
     devid: d
   
   })
-  
+}
